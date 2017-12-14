ca16a5cadfae411ff2de9cbff2573788db8f7ab0
Andrew Jarombek <ajarombek95@gmail.com>
2017-12-13 23:12:03 -0500

    D16.0 - Begin MongoDB Work

---
+++ Discoveries/2017/12-Dec/12-14-MongoDB-Pt1/Source/create.js
@@ -0,0 +1,5 @@
+// Author: Andrew Jarombek
+// Date: 12/13/2017
+// MongoDB JavaScript Shell commands for creating a christmas tree database
+
+db.tree.insert({type: "balsam"})
---
+++ Discoveries/2017/12-Dec/12-14-MongoDB-Pt1/Source/setup.sh
@@ -0,0 +1,5 @@
+sudo mongo
+
+# Executed within the mongodb shell in bash
+# Change the database - a namespace in mongodb
+use xmas
