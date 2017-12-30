20a9b33f87b8f73e2d86e0cb9d15a2f630c848ea
Andrew Jarombek <ajarombek95@gmail.com>
2017-12-30 15:11:47 -0500

    B1.1 - NodeJS MongoDB Prototype Blog [Prototype Structure] Done

--- sass.sh
+++ sass.sh
@@ -4,3 +4,6 @@ gem install sass
 
 # Watch the sass file for changes and convert it to css
 sass --watch Sass/blog-discovery.scss:blog-discover.css
+
+# Also compress the generated css file
+sass --watch --style compact Sass/blog-discovery.scss:blog-discover.css &
