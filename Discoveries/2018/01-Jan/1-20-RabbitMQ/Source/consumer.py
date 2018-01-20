075cc5f06421d2e3092d390dc7758c9c14bc2d8c
Andrew Jarombek <ajarombek95@gmail.com>
2018-01-20 15:53:29 -0500

    D25.1 - More RabbitMQ config - fully durable

--- Discoveries/2018/01-Jan/1-20-RabbitMQ/Source/consumer.py
+++ Discoveries/2018/01-Jan/1-20-RabbitMQ/Source/consumer.py
@@ -10,23 +10,55 @@ import sys
 connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
 channel = connection.channel()
 
-channel.exchange_declare(exchange='topic_log', exchange_type='topic')
+# Create an exchange if it does not already exist, otherwise make sure it has expected attributes
+# exchange -- the name of the exchange
+# exchange_type -- the type of exchange, in this case a topic exchange
+# passive -- perform an exchange declare (False) or just check if it exists (True)
+# durable -- If the exchange will survive a RabbitMQ reboot
+# auto_delete -- If the exchange will be removed when no queues are bound to it
+channel.exchange_declare(exchange='topic_log', exchange_type='topic', passive=False,
+                         durable=True, auto_delete=False)
 
-result = channel.queue_declare(exclusive=True)
+# Declare a queue and create it if needed
+# queue --  the name of the queue - if you dont specify a name RabbitMQ will auto create one
+# exclusive -- only allow access by the current connection
+# durable -- Survives a reboot
+result = channel.queue_declare(queue='log', exclusive=True, durable=True)
 queue_name = result.method.queue
 
+# Use command line arguments to get the binding keys
 binding_keys = sys.argv[1:]
 if not binding_keys:
     sys.stderr.write("Usage: %s [binding_key]...\n" % sys.argv[0])
     sys.exit(1)
 
+# Bind this channel to potentially many queues
 for binding_key in binding_keys:
+
+    # Bind a queue to a specific exchange
+    # exchange -- the exchange to bind to
+    # queue -- the queue to bind to the exchange
+    # routing_key -- the routing key to bind to - this is for use in a topic exchange
     channel.queue_bind(exchange='topic_log', queue=queue_name, routing_key=binding_key)
 
 print(" [*] Waiting for logs.  To exit press CTRL+C")
 
-def callback(ch, method, properties, body):
-    print(" [x] %r:%r" % (method.routing_key, body))
 
-channel.basic_consume(callback, queue=queue_name, no_ack=True)
+def consumer(ch, method, properties, body):
+    """ Consumer callback is called when a message is consumed """
+    if body == 'quit':
+
+        # Cancel a consumer
+        # consumer_tag -- id for the consumer
+        ch.basic_cancel(consumer_tag='consumer')
+        ch.stop_consuming()
+    else:
+        print(" [x] %r:%r" % (method.routing_key, body))
+    return
+
+# Start a queue consumer and bind messages to the callback function
+# queue -- the name of the queue to consume from
+# consumer_tag -- the id of the consumer
+# no_ack -- tell the broker not to expect a response back
+channel.basic_consume(consumer, queue=queue_name, consumer_tag='consumer', no_ack=True)
 channel.start_consuming()
--- Discoveries/2018/01-Jan/1-20-RabbitMQ/Source/producer.py
+++ Discoveries/2018/01-Jan/1-20-RabbitMQ/Source/producer.py
@@ -10,12 +10,26 @@ import sys
 connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
 channel = connection.channel()
 
-channel.exchange_declare(exchange='topic_log', exchange_type='topic')
+# Declare the topic_log exchange
+channel.exchange_declare(exchange='topic_log', exchange_type='topic', passive=False,
+                         durable=True, auto_delete=False)
 
 routing_key = sys.argv[1] if len(sys.argv) > 2 else 'default'
 message = ' '.join(sys.argv[2:]) or 'default'
 
-channel.basic_publish(exchange='topic_log', routing_key=routing_key, body=message)
+# Set the content type for the RabbitMQ channel
+# Also set the delivery mode to persistent - this allows the message to survive a crash
+msg_props = pika.BasicProperties()
+msg_props.content_type = 'text/plain'
+msg_props.delivery_mode = 2
+
+# Publish to the channel
+# exchange -- the exchange to publish to
+# routing_key -- the routing key to bind to
+# properties -- basic properties of the message
+# body -- the message contents
+channel.basic_publish(exchange='topic_log', routing_key=routing_key,
+                      properties=msg_props, body=message)
 print(" [x] Sent %r:%r" % (routing_key, message))
 
 connection.close()
