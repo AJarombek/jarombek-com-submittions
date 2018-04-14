bf5c35ffda804425d1ca474f1471e3ee8dd2248a
Andrew Jarombek <ajarombek95@gmail.com>
2018-04-13 21:55:34 -0400

    Refactor For Source Code Only

--- sass.sh
+++
@@ -1,9 +0,0 @@
-!/usr/bin/env bash
-
-gem install sass
-
-# Watch the sass file for changes and convert it to css
-sass --watch Sass/blog-discovery.scss:blog-discover.css
-
-# Also compress the generated css file
-sass --watch --style compact Sass/blog-discovery.scss:blog-discover.css &
