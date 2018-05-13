acae46fa2373d50a10286c19a05b9594ee194608
Andrew Jarombek <ajarombek95@gmail.com>
2018-05-13 16:05:54 -0400

    Finish Generics Complexities

--- 2018/05-May/5-13-Generics-Arrays-Complexities-Java/Main.java
+++ 2018/05-May/5-13-Generics-Arrays-Complexities-Java/Main.java
@@ -8,8 +8,8 @@ public class Main {
     public static void main(String... args) {
 
         // Will Compile and Run, but will throw a java.lang.ArrayStoreException
-        Number[] arr = new Integer[1];
-        // arr[0] = 1.6;
+        Number[] array = new Integer[1];
+        // array[0] = 1.6;
 
         // Wont Compile
         // List<Number> numbers = new ArrayList<Integer>();
@@ -18,20 +18,38 @@ public class Main {
         // List<Number>[] arr1 = new ArrayList<>()[10];
         // List<?>[] array = new ArrayList<String>[10];
 
-        List<Number> n1 = new ArrayList<Number>();
+        List<Number> numberList = new ArrayList<Number>();
+
+        // Java 7 introduced the diamond operator, allowing the
+        // compiler to infer the type based on the definition
+        List<Number> numberList2 = new ArrayList<>();
+
         // List<Number> n2 = new ArrayList<Integer>(); Incompatible Types
 
         // Unbounded wildcard types are reifiable - they enforce their types at runtime.
         // These are the only generics that are reifiable, the rest are all non-reifiable
-        List<?> n2 = new ArrayList<>();
-        List<?> n3 = new ArrayList<Integer>();
+        List<?> uwList = new ArrayList<>();
+        List<?> uwList2 = new ArrayList<Integer>();
+
+        /* Invalid operation */
+        // uwList.add("hi");
+        // uwList2.add(1);
+
+        List<?> uwList3 = new ArrayList<String>(List.of("hi", "there"));
+
+        /* Invalid operation */
+        // String hi = uwList.get(0);
+
+        var hi = uwList3.get(0);
+        Object there = uwList3.get(1);
 
         List<? extends Number> n4 = new ArrayList<Number>();
         List<? extends Number> n5 = new ArrayList<Integer>();
+        // List<? extends Number> n6 = new ArrayList<Object>(); Incompatible Types
 
-        List<? super Number> n6 = new ArrayList<Number>();
-        // List<? super Number> n7 = new ArrayList<Integer>(); Incompatible Types
-        List<? super Number> n7 = new ArrayList<Object>();
+        List<? super Number> n7 = new ArrayList<Number>();
+        // List<? super Number> n8 = new ArrayList<Integer>(); Incompatible Types
+        List<? super Number> n8 = new ArrayList<Object>();
 
         List<? extends Number> numbers = new ArrayList<>(List.of(1,4,6));
 
