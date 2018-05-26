d05f8831018d1e669c44bc93a0ca1604cdd923e6
Andrew Jarombek <ajarombek95@gmail.com>
2018-05-26 16:55:46 -0400

    Finish NodeJS Command Line

--- 2018/05-May/5-26-NodeJS-Command-Line/index.js
+++ 2018/05-May/5-26-NodeJS-Command-Line/index.js
@@ -8,6 +8,7 @@
 
 const program = require('commander');
 
+// Help message to be displayed when using the --help option
 const helpMessage = `
   Examples:
     "Return a random floating point number between 0 and 1"
@@ -17,21 +18,26 @@ const helpMessage = `
       random 1 10 -t int
 `;
 
+// Declare the command line program
 program
     .version('1.0')
     .arguments('<start> <end>')
     .option('-t, --type <type>', 'Type of Number')
     .action((start, end) => {
+
+        // Log out the arguments and optional options
         console.log(`Start: ${start}`);
         console.log(`End: ${end}`);
         console.log(`Type: ${program.type}`);
 
+        // Generate the random number
         const range = end - start;
 
         const random = Math.random();
         const randomInRange = random * range;
         const randomInRangeFromStart = randomInRange + start;
 
+        // Round the random number if the int option is specified
         if (program.type && (program.type.toLowerCase() === 'int' ||
             program.type.toLowerCase() === 'integer')) {
 
--- 2018/05-May/5-26-NodeJS-Command-Line/setup.sh
+++ 2018/05-May/5-26-NodeJS-Command-Line/setup.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+# Setup for the command line program
 yarn init
 npm install -g
 yarn add commander
+
+# Execute the command line program
+random --help
+random 0 1
+random 1 10 -t int
