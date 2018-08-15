d865fc1c6896dede973f2e022d0e47ec9c729ec8
Andrew Jarombek <ajarombek95@gmail.com>
2018-08-14 21:38:51 -0400

    More Groovy Closure Exploration

--- 2018/08-Aug/8-15-groovy-closures/groovy/closures.groovy
+++ 2018/08-Aug/8-15-groovy-closures/groovy/closures.groovy
@@ -13,8 +13,9 @@ list.each { println it }
 /**
  * Create a lottery scenario where the first item in a list has the greatest probability
  * of winning and the last item has the smallest probability.
- * @param list
- * @param sortingLogic
+ * @param list - a list of items, one of which will win the lottery
+ * @param sortingLogic - logic to sort the list of items - remember that the first item
+ * in the list has the greatest chance of winning!
  */
 static def lottery(List list, Closure sortingLogic) {
 
@@ -41,5 +42,69 @@ static def lottery(List list, Closure sortingLogic) {
     return sortedList[0]
 }
 
-def alphabeticalLottery = lottery(list) { a,b -> a <=> b }
-println "Lottery Winner: $alphabeticalLottery"
+/**
+ * Simulate multiple lotteries (1000) to look at the trending winners of the lottery
+ * @param list - a list of items, one of which will win each lottery
+ * @param sortingLogic - logic to sort the list of items - the first item in the list has the
+ * greatest chance of winning!
+ * @return a map where the key is one of the items entered in the lottery and the key is the number
+ * of times that item won the lottery
+ */
+static def lotterySimulator(List list, Closure sortingLogic) {
+
+    def winners = [:]
+    1000.times {
+        def winner = lottery(list, sortingLogic)
+        def currentWinCount = winners.getOrDefault(winner, 0)
+        winners.put(winner, currentWinCount + 1)
+    }
+
+    return winners
+}
+
+// Test a lottery where the list is sorted in alphabetical order
+def alphabeticalLotteryWinner = lottery(list) { a,b -> a <=> b }
+println "Lottery Winner: $alphabeticalLotteryWinner"
+
+def simulation = lotterySimulator(list, { a,b -> a <=> b })
+println simulation.sort { a,b -> b.value <=> a.value }
+
+/**
+ * Class that defines methods for sorting a list entered in a lottery
+ */
+class SortingLogic {
+
+    /**
+     * A basic comparison function between two items
+     * @param a - first item to compare
+     * @param b - second item to compare
+     * @return 1 if the first value is larger, 0 if they are equal, and -1 if the second value is larger
+     */
+    static int basic(a, b) {
+        return a <=> b
+    }
+
+    /**
+     * A comparison function between two items based on the number of characters in each
+     * @param a - first item to compare
+     * @param b - second item to compare
+     * @return 1 if the first value is larger, 0 if they are equal, and -1 if the second value is larger
+     */
+    static int length(String a, String b) {
+        return a.length() <=> b.length()
+    }
+}
+
+// Test a lottery where the list is sorted in a basic order
+def basicLotteryWinner = lottery(list, SortingLogic.&basic)
+println "Basic Lottery Winner: $basicLotteryWinner"
+
+def basicSimulation = lotterySimulator(list, SortingLogic.&basic)
+println basicSimulation.sort { a,b -> b.value <=> a.value }
+
+// Test a lottery where the list is sorted by the length of the string elements
+def stringLengthLotteryWinner = lottery(list, SortingLogic.&length)
+println "String Length Lottery Winner: $stringLengthLotteryWinner"
+
+def stringLengthSimulation = lotterySimulator(list, SortingLogic.&length)
+println stringLengthSimulation.sort { a,b -> b.value <=> a.value }
