123ae32c2172fc305caede425b853b3ee14b637d
Andrew Jarombek <ajarombek95@gmail.com>
2018-08-23 21:07:47 -0400

    More work on Currying & Haskell Basics

--- 2018/08-Aug/8-27-groovy-currying/advancedCurry.groovy
+++ 2018/08-Aug/8-27-groovy-currying/advancedCurry.groovy
@@ -6,31 +6,50 @@
  * @since 8/22/2018
  */
 
-class Pace {
+/**
+ * Time class that represents a running pace/time in minutes and seconds
+ */
+class Time {
     int minutes
     int seconds
 
-    static int milePace(double miles, Pace pace) {
-        def seconds = (pace.minutes * 60) + pace.seconds
+    /**
+     * Calculate the mile pace for a run given a distance in miles and a certain minutes and seconds
+     * @param miles - the mileage of the run
+     * @param time - the time it took to complete the run
+     * @return the pace of the run
+     */
+    static int milePace(double miles, Time time) {
+        def seconds = (time.minutes * 60) + time.seconds
         return seconds / miles
     }
 
     @Override
     String toString() {
-        return "Pace: $minutes:${seconds >= 10 ? seconds : "0${seconds}"}"
+        return "Time: $minutes:${seconds >= 10 ? seconds : "0${seconds}"}"
     }
 }
 
-def toPace = {int seconds -> new Pace(minutes: Math.floor(seconds / 60), seconds: seconds % 60)}
+// Closure to convert an integer representing seconds into a Time object
+def toPace = {int seconds -> new Time(minutes: Math.floor(seconds / 60), seconds: seconds % 60)}
 
-def pace = Pace.&milePace
+// Use the milePace() static method as a closure.  This gives us access to the curry() method
+def pace = Time.&milePace
 
+// Curry the pace closure to calculate paces for 5K races
 def pacePer5K = pace.curry(3.106)
 
+// The purpose of a composition closure is to combine two other closures
 def composition = { f, g, x -> f(g(x)) }
 
+// Create a composition of the pacePer5K closure and the toPace closure.  The resulting closure takes in a
+// 5K time and returns the pace of the 5K represented by a Time object.
 def minutesPerMileFor5K = composition.curry(toPace, pacePer5K)
 
-def pr = [minutes: 15, seconds: 27] as Pace
+def pr = [minutes: 15, seconds: 27] as Time
 
-println minutesPerMileFor5K(pr)
+assert minutesPerMileFor5K(pr).toString() == "Time: 4:58"
+
+def trackTT = [minutes: 16, seconds: 42] as Time
+
+assert minutesPerMileFor5K(trackTT).toString() == "Time: 5:22"
--- Unknown/haskell/basics.hs
+++ Unknown/haskell/basics.hs
@@ -1,5 +1,30 @@
+#!/usr/bin/env stack
+-- stack --install-ghc runghc
+
 {-
-Explore the basics of Haskell
-Author: Andrew Jarombek
-Date: 8/24/2018
+  Getting started with Haskell
+  Author: Andrew Jarombek
+  Date: 8/23/2018
 -}
+
+-- Function to subtract one integer from another.  Not that the type definition appears as a higher order function,
+-- however 'Int -> Int -> Int' is equivalent to a function with two parameters: 'Int, Int -> Int'
+-- Ex. sub 5 3
+sub :: Int -> Int -> Int
+sub x y = x-y
+
+-- Function to subtract one integer from another from the first two items in a list
+-- Ex: subList [5,3]
+subList :: [Int] -> Int
+subList [x, y] = x-y
+
+-- Function that takes in a tuple type.  The second tuple component is subtracted from the second tuple component.
+-- Both components are integers.
+-- Ex: subTuple (4,5)
+subTuple :: (Int, Int) -> Int
+subTuple (x,y) = x-y
+
+-- Display the program result in stdout
+-- 'show :: Int -> String' converts a value to a character list (which is aliased as String)
+main :: IO ()
+main = putStrLn (show (sub 5 3))
