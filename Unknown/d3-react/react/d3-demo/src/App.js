c520af017237b5a4d92387a1114908734353bdc3
Andrew Jarombek <ajarombek95@gmail.com>
2018-09-26 22:34:47 -0400

    More Featured D3 Bar Graph

--- Unknown/d3-react/react/d3-demo/src/App.js
+++ Unknown/d3-react/react/d3-demo/src/App.js
@@ -2,14 +2,42 @@ import React from 'react';
 import Graph from './Graph';
 import './App.css';
 
+/**
+ * App Component
+ * @author Andrew Jarombek
+ * @since 9/26/2018
+ */
+
+// Data to pass to the D3 Graph
 const data = [
-    { value: 2 },
-    { value: 4.3 },
-    { value: 0 },
-    { value: 4.9 },
-    { value: 3.3 },
-    { value: 7 },
-    { value: 5.5 }
+    {
+        miles: 2,
+        feel: 5
+    },
+    {
+        miles: 4.3,
+        feel: 7
+    },
+    {
+        miles: 1,
+        feel: 3
+    },
+    {
+        miles: 4.9,
+        feel: 2
+    },
+    {
+        miles: 3.3,
+        feel: 8
+    },
+    {
+        miles: 7,
+        feel: 5
+    },
+    {
+        miles: 5.5,
+        feel: 6
+    }
 ];
 
 const App = () =>
--- Unknown/d3-react/react/d3-demo/src/Graph.js
+++ Unknown/d3-react/react/d3-demo/src/Graph.js
@@ -4,7 +4,31 @@ import PropTypes from "prop-types";
 import * as d3 from 'd3';
 import {withFauxDOM} from 'react-faux-dom';
 
+/**
+ * Graph Component
+ * @author Andrew Jarombek
+ * @since 9/26/2018
+ */
+
 class Graph extends React.Component {
+
+    constructor() {
+        super();
+
+        // Colors for the bars which are indexed based on the feel property
+        this.colors = [
+            'rgba(204, 0, 0, .4)',
+            'rgba(255, 51, 0, .4)',
+            'rgba(204, 102, 0, .4)',
+            'rgba(255, 153, 0, .4)',
+            'rgba(255, 255, 51, .4)',
+            'rgba(187, 187, 187, .4)',
+            'rgba(115, 230, 0, .4)',
+            'rgba(0, 153, 0, .4)',
+            'rgba(0, 102, 0, .4)',
+            'rgba(26, 26, 255, .4)'
+        ];
+    }
 
     static propTypes = {
         data: PropTypes.array.isRequired
@@ -19,9 +43,13 @@ class Graph extends React.Component {
         const faux = this.props.connectFauxDOM('div', 'chart');
 
         const graphWidth = 800;
-        const graphHeight = 150;
+        const graphHeight = 300;
         const graphPaddingRight = 4;
         const graphData = this.props.data;
+
+        const heightScale = d3.scaleLinear()
+            .domain([0, d3.max(graphData, (d) => d.miles)])
+            .range([0, graphHeight]);
 
         const svg = d3.select(faux)
             .append("svg")
@@ -33,10 +61,23 @@ class Graph extends React.Component {
             .enter()
             .append("rect")
             .attr("x", (d, i) => i * (graphWidth / graphData.length))
-            .attr("y", (d) => graphHeight - (d.value * 10))
+            .attr("y", (d) => graphHeight - heightScale(d.miles))
             .attr("width", graphWidth / graphData.length - graphPaddingRight)
-            .attr("height", (d) => d.value * 10)
-            .text((d) => `${d.value}`);
+            .attr("height", (d) => heightScale(d.miles))
+            .attr("fill", (d) => this.colors[d.feel])
+            .text((d) => `${d.miles}`);
+
+        svg.selectAll("text")
+            .data(graphData)
+            .enter()
+            .append("text")
+            .text((d) => d.miles)
+            .attr("x", (d, i) =>
+                i * (graphWidth / graphData.length) +
+                    (graphWidth / graphData.length - graphPaddingRight) / 2
+            )
+            .attr("y", (d) => graphHeight - heightScale(d.miles) + 18)
+            .attr("class", "graph-label");
 
         this.props.animateFauxDOM(800);
     }
