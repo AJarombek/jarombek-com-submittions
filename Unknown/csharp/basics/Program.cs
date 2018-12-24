1e6c5c8bd3d32e4805ded85dad1aab61569ecbed
Andrew Jarombek <ajarombek95@gmail.com>
2018-12-23 19:04:34 -0500

    Haskell and C# Work

---
+++ Unknown/csharp/basics/Program.cs
@@ -0,0 +1,33 @@
+﻿/**
+ * C# Basics
+ * Author: Andrew Jarombek
+ * Date: 12/23/2018
+ */
+
+using System;
+using System.Diagnostics;
+
+namespace Basics
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            // C# has support for tuples
+            var name = ("Andrew", "Jarombek");
+            Console.WriteLine(name.Item1);
+            Console.WriteLine(name.Item2);
+
+            Debug.Assert(name.Item1 == "Andrew" && name.Item2 == "Jarombek");
+            
+            // ...and tuples with named items
+            var info = (name: "Andy", age: 23);
+            Debug.Assert(info.name == "Andy" && info.age == 23);
+
+            // You can create an identifier with a keyword if its prefixed with '@'
+            var @int = 5;
+
+            Debug.Assert(@int == 5);
+        }
+    }
+}
--- Unknown/haskell/inheritance.hs
+++ Unknown/haskell/inheritance.hs
@@ -6,11 +6,17 @@
 
 -- Types of fir trees
 data FrasierFir = FrasierFir Int Int
+                  deriving Show
+
 data BalsamFir = BalsamFir Int Int
+                 deriving Show
+
 data DouglasFir = DouglasFir Int Int
+                  deriving Show
 
 -- Grades for different qualities of the fir trees
 data Grade = Fair | Good | Excellent
+             deriving Show
 
 {-|
  - A class for a biological tree.  Each tree must be able to calculate its height
@@ -23,7 +29,7 @@ class Tree a where
   height _ = 0
 
 {-|
- - A class for a Christmas tree that inherits from the biological tree class.
+ - A class for a Christmas tree which is an extension of the biological tree class.
  -}
 class (Tree a) => ChristmasTree a where
   -- Class operator
@@ -33,7 +39,7 @@ class (Tree a) => ChristmasTree a where
   holiday_tree _ = True
 
 {-|
- - A class for an evergreen tree that inherits from the biological tree class.
+ - A class for an evergreen tree which is an extension of the biological tree class.
  -}
 class (Tree a) => EvergreenTree a where
   -- Class operator
@@ -43,7 +49,7 @@ class (Tree a) => EvergreenTree a where
   leaf_persistence _ = True
 
 {-|
- - A class for a fir tree, which is both a Christmas tree and an evergreen tree.
+ - A class for a fir tree, which is an extension of both a Christmas tree and an evergreen tree.
  -}
 class (ChristmasTree a, EvergreenTree a) => FirTree a where
   -- Class operators
@@ -52,30 +58,73 @@ class (ChristmasTree a, EvergreenTree a) => FirTree a where
   needle_retention :: a -> Grade
 
 {-|
- - FrasierFir is an instance of FirTree (which inherits Tree, ChristmasTree, and EvergreenTree)
+ - FrasierFir is an instance of FirTree.  Since it is an instance of FirTree, it must also be an instance
+ - of ChristmasTree, EvergreenTree, and Tree
  -}
 instance FirTree FrasierFir where
   fragrance a = Fair
   ease_to_decorate a = Good
   needle_retention a = Excellent
 
+instance ChristmasTree FrasierFir
+instance EvergreenTree FrasierFir
+
+instance Tree FrasierFir where
+  height (FrasierFir x y) = (x * 12) + y
+
 {-|
- - DouglasFir is an instance of FirTree (which inherits Tree, ChristmasTree, and EvergreenTree)
+ - DouglasFir is an instance of FirTree.  Since it is an instance of FirTree, it must also be an instance
+ - of ChristmasTree, EvergreenTree, and Tree
  -}
 instance FirTree DouglasFir where
   fragrance a = Good
   ease_to_decorate a = Fair
   needle_retention a = Excellent
 
+instance ChristmasTree DouglasFir
+instance EvergreenTree DouglasFir
+
+instance Tree DouglasFir where
+  height (DouglasFir x y) = (x * 12) + y
+
 {-|
- - BalsamFir is an instance of FirTree (which inherits Tree, ChristmasTree, and EvergreenTree)
+ - BalsamFir is an instance of FirTree.  Since it is an instance of FirTree, it must also be an instance
+ - of ChristmasTree, EvergreenTree, and Tree
  -}
 instance FirTree BalsamFir where
   fragrance a = Excellent
   ease_to_decorate a = Excellent
   needle_retention a = Fair
 
+instance ChristmasTree BalsamFir
+instance EvergreenTree BalsamFir
+
+instance Tree BalsamFir where
+  height (BalsamFir x y) = (x * 12) + y
+
 {- Main Function -}
 main :: IO ()
 main = do
-  print $ ""
+  let frasier = FrasierFir 7 2
+
+  print $ frasier -- FrasierFir 7 2
+  print $ fragrance frasier -- Fair
+  print $ ease_to_decorate frasier -- Good
+  print $ needle_retention frasier -- Excellent
+  print $ leaf_persistence frasier -- True
+  print $ holiday_tree frasier -- True
+  print $ height frasier -- 86
+
+  let balsam = BalsamFir 5 6
+
+  print $ balsam -- BalsamFir 5 6
+  print $ fragrance balsam -- Excellent
+  print $ ease_to_decorate balsam -- Excellent
+  print $ needle_retention balsam -- Fair
+
+  let douglas = DouglasFir 10 2
+
+  print $ douglas -- DouglasFir 10 2
+  print $ fragrance douglas -- Good
+  print $ ease_to_decorate douglas -- Fair
+  print $ needle_retention douglas -- Excellent
