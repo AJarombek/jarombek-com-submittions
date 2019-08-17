a1a452a73e7ffa5b938cf8b78fc36c1bf936756e
AJarombek <ajarombek95@gmail.com>
2019-08-17 12:59:18 -0400

    Move C# equality from misc-code-samples, add struct equality

---
+++ 2019/08-Aug/08-18-language-equality/csharp/equality/Program.cs
@@ -0,0 +1,16 @@
+﻿/**
+ * Investigate equality in C# and the .NET Framework
+ * Author: Andrew Jarombek
+ * Date: 8/17/2019
+ */
+
+namespace equality
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            Equality.Execute();
+        }
+    }
+}
---
+++ Unknown/csharp/misc/Program.cs
@@ -0,0 +1,41 @@
+﻿/**
+ * Investigate basic C# class concepts which will help creating an API
+ * Author: Andrew Jarombek
+ * Date: 2/4/2019
+ */
+
+using System;
+using static System.Diagnostics.Debug;
+
+namespace ClassConcepts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Testing Index.cs */
+            
+            // Prove the static constructor is invoked just prior to the type being used.
+            Assert(Index<int>.Ready);
+            Assert(Index<object>.Ready);
+            
+            var indexCollection = new Index<string>();
+            indexCollection[0] = "Andy";
+            
+            Assert(indexCollection[0].Equals("Andy"));
+
+            try
+            {
+                indexCollection[2] = "Tom";
+                
+                // Should not reach this line
+                Assert(false);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: Must insert at a valid index.");
+            }
+            
+        }
+    }
+}
