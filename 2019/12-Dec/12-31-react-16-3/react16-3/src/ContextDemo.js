f34f4e7b666dcf586a317ff87cf65f2664f50c80
Andrew Jarombek <ajarombek95@gmail.com>
2020-01-12 12:19:53 -0500

    wrapping up context API

--- 2019/12-Dec/12-31-react-16-3/react16-3/src/ContextDemo.js
+++ 2019/12-Dec/12-31-react-16-3/react16-3/src/ContextDemo.js
@@ -11,7 +11,8 @@ import FeaturePage from './FeaturePage';
 import ThemeWithProps from './ThemeWithProps';
 import ThemeWithContext from './ThemeWithContext';
 
-const themeWithPropsCode = `// ThemeWithProps.js
+const themeWithPropsCode =
+`// ThemeWithProps.js
 
 import React, {useState} from 'react';
 import {useHistory} from 'react-router-dom';
@@ -37,10 +38,12 @@ const ThemeWithProps = () => {
     </div>
   );
 };
+`;
 
-const ThemeWithPropsCard = ({theme}) => {
+const themeWithPropsCardCode =
+`const ThemeWithPropsCard = ({theme}) => {
   return (
-    <div className={classnames('theme-with-props-card', \`\${theme}-theme-with-props-card\`)}>
+    <div className={classnames('theme-with-props-card', \`$\{theme}-theme-with-props-card\`)}>
       <AJTextCard
         title="Theme implemented with props"
         content="... content ..."
@@ -53,7 +56,8 @@ const ThemeWithPropsCard = ({theme}) => {
 };
 `;
 
-const themeWithContextCode = `// ThemeWithContext.js
+const themeWithContextCode =
+`// ThemeWithContext.js
 
 import React, { createContext, useContext, useState } from 'react';
 import { useHistory } from 'react-router-dom';
@@ -83,8 +87,10 @@ const ThemeWithContext = () => {
     </div>
   );
 };
+`;
 
-const ThemeWithContextCard = () => {
+const themeWithContextCardCode =
+`const ThemeWithContextCard = () => {
   const context = useContext(ThemeContext);
 
   return (
@@ -109,12 +115,12 @@ const ContextDemo = () => {
         <p>
           In React 16.3, the Context API was rewritten. Context allows data to be shared
           amongst components without passing it via props from parents to children<sup>1</sup>.
-          While I'm still learning which scenarios are appropriate for the context API, the React
+          While I'm still learning which scenarios are appropriate for the Context API, the React
           blog recommends to use it for "global" data used by a tree of React
           components<sup>2</sup>.
         </p>
         <p>
-          One good use case for the context API is setting the stylistic theme of a web page based
+          One good use case for the Context API is setting the stylistic theme of a web page based
           on a toggleable switch.  All the components on a page should be aware of the theme chosen,
           but ideally it doesn't need to be passed to every component via a prop.
         </p>
@@ -123,19 +129,59 @@ const ContextDemo = () => {
         </p>
         <ThemeWithProps />
         <p>
-          Next I implemented the same component with the context API.
+          Next I implemented the same component with the Context API.
         </p>
         <ThemeWithContext />
         <p>
           Both components are functionally equivalent.  To identify the differences between the two
-          components, let's take a look at the the code.
+          components, let's take a look at the the code.  First, here is the code for a theme
+          configured with props:
         </p>
         <AJCodeSnippet language="javascript">
           {themeWithPropsCode}
         </AJCodeSnippet>
+        <AJCodeSnippet>
+          {themeWithPropsCardCode}
+        </AJCodeSnippet>
+        <p>
+          Next, here is the code for a theme configured with the Context API:
+        </p>
         <AJCodeSnippet language="javascript">
           {themeWithContextCode}
         </AJCodeSnippet>
+        <AJCodeSnippet>
+          {themeWithContextCardCode}
+        </AJCodeSnippet>
+        <p>
+          If you want to see the full code, <a href="https://bit.ly/35Mto2s">
+          ThemeWithProps.js</a> and <a href="https://bit.ly/2ThDS7q">ThemeWithContext.js</a> are
+          available on GitHub.  In these code samples, its important to focus on how the
+          <code>theme</code> variable is passed down to the <code>ThemeWithPropsCard</code> and
+          <code>ThemeWithContextCard</code> components.
+        </p>
+        <p>
+          <code>ThemeWithPropsCard</code> receives the selected theme via a prop named
+          <code>theme</code>.  The parent component explicitly passes the theme to the child
+          component.  In this scenario passing the theme to a single child wasn't much of a hassle.
+          However, imagine a chain of nested components, all of which must forward the
+          <code>theme</code> prop down to <code>ThemeWithPropsCard</code>.  Components in the middle
+          of this chain may not even use the <code>theme</code> prop, but need to be aware of it
+          because a nested child needs it.
+        </p>
+        <p>
+          Ideally components only contain code that helps them complete their job.  Creating this
+          separation of concerns is possible with the Context API.  With the Context API, global
+          variables are created and provided for component chains.  Components can consume the
+          context if they need their values, or can just ignore them.  In the previous hypothetical
+          example I presented, Components in the middle of the chain don't need to be aware of the
+          context they exist in.
+        </p>
+        <p>
+          In <strong>ThemeWithContext.js</strong> I created a context by calling
+          <code>createContext()</code>, which is a method imported from React<sup>3</sup>.  The
+          argument passed to <code>createContext()</code> is the default value of the context when a
+          component does not have a context provider above it in the component hierarchy.
+        </p>
       </div>
     </FeaturePage>
   );
